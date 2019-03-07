e6426e706e91b4c2748a4aa55648b2512c663c93
Marcin Konopka <marcin.konopka00@gmail.com>
2019-03-07 18:27:15 +0100

    lab0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,18 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
-    }
+        if(value == null) {return Collections.emptyList(); }
+        ArrayList<String> arrayList = new ArrayList<String>();
 
+        for (int i = 0; i < value.length(); i++) { arrayList.add(value.substring(i)); }
+        arrayList.add("");
+
+        return arrayList;
+    }
 }
